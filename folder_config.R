b4d49e3e8efdd0599270e62c17c31fa81b32d566
in-code-nito <56764675+in-code-nito@users.noreply.github.com>
2019-11-04 18:04:43 -0500

    Update folder_config.R

--- folder_config.R
+++ folder_config.R
@@ -1,81 +1,20 @@
 # local configuration hack
 
-if (Sys.info ()["user"] == "Brian") {
-  # For Brian
-  constants <- "C:/Users/Brian/Dropbox/Sim/Constants/"
-  output <- "C:/Users/Brian/Dropbox/Sim/2018 Outcomes/"
-  gamefiles <- "C:/Users/Brian/Dropbox/sim/Model/gamedata/"
-  gamedates <- "C:/Users/Brian/Dropbox/sim/Model/constants/"
-  ModelLocation <- "C:/Users/Brian/Dropbox/Sim/Model/"
-  mljcode <- "C:/Users/Brian/Dropbox/Sim/"
-}
-
-if (Sys.info()["user"] == "mlopez1") {
-  # For Mike
-  constants <- "~/Dropbox/Sim/Constants/"
-  output <- "~/Dropbox/Sim/2018 Outcomes/"
-  gamefiles <- "~/Dropbox/sim/Model/gamedata/"
-  gamedates <- "~/Dropbox/sim/Model/constants/"
-  ModelLocation <- "~/Dropbox/Sim/Model/"
-  mljcode <- "~/Dropbox/Sim/"
-}
-
-
-if (Sys.info()["user"] == "elopez") {
-  # For Erin
-  constants <- "C:/Users/elopez/Constants/"
-  output <- "C:/Users/elopez/Dropbox/2018 Outcomes/"
-  gamefiles <- "C:/Users/elopez/Dropbox/Sim/Model/gamedata/"
-  gamedates <- "C:/Users/elopez/Dropbox/Sim/Model/constants/"
-  ModelLocation <- "C:/elopez/Dropbox/Sim/Model/"
-  mljcode <- "C:/elopez/Dropbox/Sim/"
-}
-print('test')
-if (Sys.info ()["user"] == "jenkins") {
-  # For Apple Sim Box
-  constants <- "/Volumes/Mac_HD/Dropbox/Sim/Constants/"
-  output <- "/Volumes/Mac_HD/Dropbox/Sim/Model/2018 Outcomes/"
-  gamefiles <- "/Volumes/Mac_HD/Dropbox/Sim/Model/gamedata/"
-  gamedates <- "/Volumes/Mac_HD/Dropbox/Sim/Model/constants/"
-  ModelLocation <- "/Volumes/Mac_HD/Dropbox/Sim/Model"
-  mljcode <- "/Volumes/Mac_HD/Dropbox/Sim"
-}
-
-if (Sys.info()["user"] == "doherbr") {
-  # For doherbr
-  constants <- "C:/Users/doherbr/desktop/Dropbox/Sim/Constants/"
-  output <- "C:/Users/doherbr/desktop/Dropbox/Sim/2018 Outcomes/"
-  gamefiles <- "C:/Users/doherbr/desktop/Dropbox/sim/Model/gamedata/"
-  gamedates <- "C:/Users/doherbr/desktop/Dropbox/sim/Model/constants/"
-  ModelLocation <- "C:/Users/doherbr/desktop/Dropbox/Sim/Model/"
-  mljcode <- "C:/Users/doherbr/desktop/Dropbox/Sim/"
-}
-
-
-
-if (Sys.info ()["user"] == "btdoh"){
-  # For brian laptop
-  constants <- "C:/Users/btdoh/Dropbox/Sim/Constants/"
-  output <- "C:/Users/btdoh/Dropbox/Sim/2018 Outcomes/"
-  gamefiles <- "C:/Users/btdoh/Dropbox/sim/Model/gamedata/"
-  gamedates <- "C:/Users/btdoh/Dropbox/sim/Model/constants/"
-  ModelLocation <- "C:/Users/btdoh/Dropbox/Sim/Model/"
-  mljcode <- "C:/Users/btdoh/Dropbox/Sim/"
-}
-
-
-
+year <- format(Sys.time(), "%Y")
 
 if (Sys.info ()["user"] == "brian") {
   # For Brian
-  constants <- "/home/brian/Dropbox/Sim/Constants/"
-  output <- "/home/brian/Dropbox/Sim/2018 Outcomes/"
-  gamefiles <- "/home/brian/Dropbox/sim/Model/gamedata/"
-  gamedates <- "/home/brian/Dropbox/sim/Model/constants/"
-  ModelLocation <- "/home/brian/Dropbox/Sim/Model/"
-  mljcode <- "/home/brian/Dropbox/Sim/"
+  constants <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy"
+  output <- paste("C:/Users/brian/Dropbox/mlb/sim/outputs/legacy/", year)
+  gamefiles <- paste("C:/Users/brian/Dropbox/mlb/sim/daily_projections/" ,year)
+  gamedates <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy"
+  ModelLocation <- "C:/Users/brian/Dropbox/mlb/sim/"
+  mljcode <- "C:/Users/brian/Dropbox/mlb/sim/scripts_r/legacy_files"
 }
 
+library("baseballr")
+library("tidyverse")
+library("lubridate")
 library(knitr)
 library(readxl)
 library(readr)
