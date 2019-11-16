9fae23847d86d72a9d82dd652a35457a00387991
in-code-nito <56764675+in-code-nito@users.noreply.github.com>
2019-11-16 17:34:10 -0500

    Update folder_config.R

--- folder_config.R
+++ folder_config.R
@@ -5,8 +5,8 @@ year <- paste(format(Sys.time(), "%Y"))
 if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Windows") {
   # For Brian laptop
   constants <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy/"
-  output <- paste("C:/Users/brian/Dropbox/mlb/sim/outputs/legacy/", year,"/", sep = '')
-  gamefiles <- paste("C:/Users/brian/Dropbox/mlb/sim/daily_projections/" ,year,"/", sep = '')
+  output <- paste("C:/Users/brian/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
+  gamefiles <- paste("C:/Users/brian/Dropbox/mlb/sim/daily_projections/" ,format(Sys.time(), "%Y"),"/", sep = '')
   gamedates <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy/"
   ModelLocation <- "C:/Users/brian/Dropbox/mlb/sim/"
   mljcode <- "C:/Users/brian/Dropbox/mlb/sim/scripts_r/legacy_files/prod/"
@@ -15,8 +15,8 @@ if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Windows") {
 if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Linux") {
   # For Brian Tower
   constants <- "/home/brian/Dropbox/mlb/sim/factors/legacy"
-  output <- paste("/home/brian/Dropbox/mlb/sim/outputs/legacy/", year,"/", sep = '')
-  gamefiles <- paste("/home/brian/Dropbox/mlb/sim/daily_projections/" ,year,"/", sep = '')
+  output <- paste("/home/brian/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
+  gamefiles <- paste("/home/brian/Dropbox/mlb/sim/daily_projections/" ,format(Sys.time(), "%Y"),"/", sep = '')
   gamedates <- "/home/brian/Dropbox/mlb/sim/factors/legacy/"
   ModelLocation <- "/home/brian/Dropbox/mlb/sim/"
   mljcode <- "/home/brian/Dropbox/mlb/sim/scripts_r/legacy_files/prod/"
@@ -25,8 +25,8 @@ if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Linux") {
 if (Sys.info()["user"] == "mlopez1") {
   # For Mike L
   constants <- "~/Dropbox/mlb/sim/factors/legacy/"
-  output <- paste("~/Dropbox/mlb/sim/outputs/legacy/", year,"/", sep = '')
-  gamefiles <- paste("~/Dropbox/mlb/sim/daily_projections/" ,year,"/", sep = '')
+  output <- paste("~/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
+  gamefiles <- paste("~/Dropbox/mlb/sim/daily_projections/" ,format(Sys.time(), "%Y"),"/", sep = '')
   gamedates <- "~/Dropbox/mlb/sim/factors/legacy/"
   ModelLocation <- "~/Dropbox/mlb/sim/"
   mljcode <- "~/Dropbox/mlb/sim/scripts_r/legacy_files/prod/"
