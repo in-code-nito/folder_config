3684b7ec816a17a82def6577df3be3a814b7d383
in-code-nito <56764675+in-code-nito@users.noreply.github.com>
2019-11-16 17:51:40 -0500

    Update folder_config.R

--- folder_config.R
+++ folder_config.R
@@ -4,7 +4,7 @@ if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Windows") {
   # For Brian laptop
   constants <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy"
   output <- paste("C:/Users/brian/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"), sep = '')
-  gamefiles <- paste("C:/Users/brian/Dropbox/mlb/sim/daily_projections/" ,format(Sys.time(), "%Y"), sep = '')
+  gamefiles <- paste("C:/Users/brian/Dropbox/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"), sep = '')
   gamedates <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy"
   ModelLocation <- "C:/Users/brian/Dropbox/mlb/sim"
   mljcode <- "C:/Users/brian/Dropbox/mlb/sim/scripts_r/legacy/prod"
@@ -14,7 +14,7 @@ if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Linux") {
   # For Brian Tower
   constants <- "/home/brian/Dropbox/mlb/sim/factors/legacy"
   output <- paste("/home/brian/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
-  gamefiles <- paste("/home/brian/Dropbox/mlb/sim/daily_projections/" ,format(Sys.time(), "%Y"),"/", sep = '')
+  gamefiles <- paste("/home/brian/Dropbox/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"),"/", sep = '')
   gamedates <- "/home/brian/Dropbox/mlb/sim/factors/legacy/"
   ModelLocation <- "/home/brian/Dropbox/mlb/sim/"
   mljcode <- "/home/brian/Dropbox/mlb/sim/scripts_r/legacy/prod/"
@@ -24,7 +24,7 @@ if (Sys.info()["user"] == "mlopez1") {
   # For Mike L
   constants <- "~/Dropbox/mlb/sim/factors/legacy/"
   output <- paste("~/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
-  gamefiles <- paste("~/Dropbox/mlb/sim/daily_projections/" ,format(Sys.time(), "%Y"),"/", sep = '')
+  gamefiles <- paste("~/Dropbox/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"),"/", sep = '')
   gamedates <- "~/Dropbox/mlb/sim/factors/legacy/"
   ModelLocation <- "~/Dropbox/mlb/sim/"
   mljcode <- "~/Dropbox/mlb/sim/scripts_r/legacy/prod/"
