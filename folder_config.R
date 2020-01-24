2425c42c320831ff6dbe6d40cfacfa84a6fecf96
in-code-nito <56764675+in-code-nito@users.noreply.github.com>
2020-01-24 17:50:35 -0500

    Update folder_config.R

--- folder_config.R
+++ folder_config.R
@@ -2,17 +2,17 @@
 
 if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Windows") {
   # For Brian laptop
-  constants <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy"
+  constants <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy/"
   output <- paste("C:/Users/brian/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
   gamefiles <- paste("C:/Users/brian/Dropbox/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"),"/", sep = '')
-  gamedates <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy"
-  ModelLocation <- "C:/Users/brian/Dropbox/mlb/sim"
-  mljcode <- "C:/Users/brian/Dropbox/mlb/sim/scripts_r/legacy/prod"
+  gamedates <- "C:/Users/brian/Dropbox/mlb/sim/factors/legacy/"
+  ModelLocation <- "C:/Users/brian/Dropbox/mlb/sim/"
+  mljcode <- "C:/Users/brian/Dropbox/mlb/sim/scripts_r/legacy/prod/"
 }
 
 if (Sys.info ()["user"] == "brian" & Sys.info()['sysname'] =="Linux") {
   # For Brian Tower
-  constants <- "/home/brian/Dropbox/mlb/sim/factors/legacy"
+  constants <- "/home/brian/Dropbox/mlb/sim/factors/legacy/"
   output <- paste("/home/brian/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
   gamefiles <- paste("/home/brian/Dropbox/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"),"/", sep = '')
   gamedates <- "/home/brian/Dropbox/mlb/sim/factors/legacy/"
@@ -32,7 +32,7 @@ if (Sys.info()["user"] == "mlopez1") {
 
 if (Sys.info ()["user"] == "root" & Sys.info()['sysname'] =="Linux") {
   # For Brian Tower
-constants <- "/mlb/mlb/sim/factors/legacy"
+constants <- "/mlb/mlb/sim/factors/legacy/"
 output <- paste("/mlb/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
 gamefiles <- paste("/mlb/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"),"/", sep = '')
 gamedates <- "/mlb/mlb/sim/factors/legacy/"
@@ -40,6 +40,15 @@ ModelLocation <- "/mlb/mlb/sim/"
 mljcode <- "/mlb/mlb/sim/scripts_r/legacy/prod/"
 }
 
+if (Sys.info ()["user"] == "msterling" & Sys.info()['sysname'] =="Linux") {
+	  # For Brian Tower
+	constants <- "/Users/msterling/Dropbox/mlb/sim/factors/legacy/"
+	output <- paste("/Users/msterling/Dropbox/mlb/sim/outputs/legacy/", format(Sys.time(), "%Y"),"/", sep = '')
+	gamefiles <- paste("/Users/msterling/Dropbox/mlb/sim/daily_projections/legacy/" ,format(Sys.time(), "%Y"),"/", sep = '')
+	gamedates <- "/Users/msterling/Dropbox/mlb/sim/factors/legacy/"
+	ModelLocation <- "/Users/msterling/Dropbox/mlb/sim/"
+	mljcode <- "/Users/msterling/Dropbox/mlb/sim/scripts_r/legacy/prod/"
+}
 
 
 library(devtools)
